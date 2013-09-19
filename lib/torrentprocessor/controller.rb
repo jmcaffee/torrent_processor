4a96c9e81cce689ff69086774e917b8cf5619df7
Jeff McAffee <jeff@ktechsystems.com>
2013-09-18 21:51:55 -0700

    Add empty method: upgradeDb
    	modified:   lib/torrentprocessor/controller.rb

--- lib/torrentprocessor/controller.rb
+++ lib/torrentprocessor/controller.rb
@@ -1,7 +1,7 @@
 ##############################################################################
 # File::    controller.rb
 # Purpose:: Main Controller object for TorrentProcessor utility
-# 
+#
 # Author::    Jeff McAffee 07/31/2011
 # Copyright:: Copyright (c) 2011, kTech Systems LLC. All rights reserved.
 # Website::   http://ktechsystems.com
@@ -12,7 +12,7 @@ require 'ktcommon/ktcmdline'
 
 
 module TorrentProcessor
-    
+
   ##########################################################################
   # Controller class
   class Controller
@@ -23,7 +23,7 @@ module TorrentProcessor
   attr_reader     :cfg
   attr_reader     :setup
   attr_reader     :database
-    
+
     ###
     # Constructor
     #
@@ -37,7 +37,7 @@ module TorrentProcessor
       @aswitch        = false
       @logfile        = "tp-processing.log"
     end
-      
+
 
     ###
     # Set the srcdir. The value is actually maintained/stored in the model.
@@ -50,8 +50,8 @@ module TorrentProcessor
       @model.srcdir = srcdirpath
       ret
     end
-      
-    
+
+
     ###
     # Assignment operator for setting the srcdir.
     # srcdirpath:: input file directory path
@@ -61,8 +61,8 @@ module TorrentProcessor
       $LOG.debug "Controller::srcdir=( #{srcdirpath} )"
       return srcdir(srcdirpath)
     end
-      
-    
+
+
     ###
     # Set the srcfile. The value is actually maintained/stored in the model.
     # srcfilepath:: source file path
@@ -74,8 +74,8 @@ module TorrentProcessor
       @model.srcfile = srcfilepath
       ret
     end
-      
-    
+
+
     ###
     # Assignment operator for setting the srcfile.
     # srcfilepath:: source file path
@@ -85,8 +85,8 @@ module TorrentProcessor
       $LOG.debug "Controller::srcfile=( #{srcfilepath} )"
       return srcfile(srcfilepath)
     end
-      
-    
+
+
     ###
     # Set the torrent state. The value is actually maintained/stored in the model.
     # state:: current torrent state
@@ -98,8 +98,8 @@ module TorrentProcessor
       @model.state = stateval
       ret
     end
-      
-    
+
+
     ###
     # Assignment operator for setting the state.
     # state:: current torrent state
@@ -109,8 +109,8 @@ module TorrentProcessor
       $LOG.debug "Controller::state=( #{stateval} )"
       return state(stateval)
     end
-      
-    
+
+
     ###
     # Set the torrent's prevstate. The value is actually maintained/stored in the model.
     # stateval:: previous torrent state
@@ -122,8 +122,8 @@ module TorrentProcessor
       @model.prevstate = stateval
       ret
     end
-      
-    
+
+
     ###
     # Assignment operator for setting the previous state.
     # stateval:: previous torrent state
@@ -133,8 +133,8 @@ module TorrentProcessor
       $LOG.debug "Controller::prevstate=( #{stateval} )"
       return prevstate(stateval)
     end
-      
-    
+
+
     ###
     # Set the torrent's msg. The value is actually maintained/stored in the model.
     # msgval:: msg value from uTorrent
@@ -146,8 +146,8 @@ module TorrentProcessor
       @model.msg = msgval
       ret
     end
-      
-    
+
+
     ###
     # Assignment operator for setting the uTorrent msg.
     # msgval:: msg value from uTorrent
@@ -157,8 +157,8 @@ module TorrentProcessor
       $LOG.debug "Controller::msg=( #{msgval} )"
       return msg(msgval)
     end
-      
-    
+
+
     ###
     # Set the torrent's label. The value is actually maintained/stored in the model.
     # labelval:: label value from uTorrent
@@ -170,8 +170,8 @@ module TorrentProcessor
       @model.label = labelval
       ret
     end
-      
-    
+
+
     ###
     # Assignment operator for setting the uTorrent label.
     # labelval:: label value from uTorrent
@@ -181,8 +181,8 @@ module TorrentProcessor
       $LOG.debug "Controller::label=( #{labelval} )"
       return label(labelval)
     end
-      
-    
+
+
     ###
     # Set the verbose flag. The flag is actually maintained/stored in the model.
     # arg:: True = verbose on
@@ -193,8 +193,8 @@ module TorrentProcessor
       @model.verbose = arg
       @setup.verbose = arg
     end
-      
-    
+
+
     ###
     # Assignment operator for setting the verbose flag.
     # arg:: True = verbose on
@@ -203,24 +203,24 @@ module TorrentProcessor
       $LOG.debug "Controller::verbose=( #{arg} )"
       return verbose(arg)
     end
-      
-    
+
+
     ###
     # Write the default config file to disk
-    # 
-    # 
+    #
+    #
     #
     def writeCfg()
       $LOG.debug "Controller::writeCfg()"
       Config.new.save
     end
-        
-      
+
+
     ###
-    # User supplied a command line argument(s). 
+    # User supplied a command line argument(s).
     # <em><b>Note:</b> that switches and options are not considered command line arguments.</em>
     #
-    # returns:: False to indicate that the application should exit. False by default. 
+    # returns:: False to indicate that the application should exit. False by default.
     #   <em><b>Note:</b> ArgumentError exception is raised as well.</em>
     #
     def processCmdLineArgs(arg)
@@ -228,8 +228,8 @@ module TorrentProcessor
       raise ArgumentError.new("Unexpected argument: #{arg}")
       return false    # Indicate that we have a problem - we are not expecting command line args.
     end
-        
-      
+
+
     ###
     # User supplied no command line arguments.
     # <em><b>Note:</b> Switches and options are not considered command line arguments.</em>
@@ -242,25 +242,25 @@ module TorrentProcessor
       return true     # Indicate that we don't care if there is no command line arg.
     end
 
-      
+
     ###
     # TODO: write process() description
     #
     def process()
       $LOG.debug "Controller::process"
-      
+
       checkSetupCompleted()
-      
+
       @model.process()
     end
-      
-    
+
+
     ###
     # Run Torrent Processor in interactive mode
     #
     def interactiveMode()
       $LOG.debug "Controller::interactiveMode"
-      
+
       if !@setup.checkSetupCompleted()
         # Tell the user to configure the application if it has not yet been configured.
         puts
@@ -273,8 +273,8 @@ module TorrentProcessor
       end
       @model.interactiveMode()
     end
-      
-    
+
+
     ###
     # Check to make sure the user has setup the application
     #
@@ -286,20 +286,23 @@ module TorrentProcessor
         puts "Run Torrent Processor with the -init option to configure it."
         exit
       end
-                
+
     end
-      
-    
+
+
     ###
     # Setup the application
     #
     def setupApp()
       $LOG.debug "Controller::setupApp"
       @setup.setupApp()
-                
+
     end
-      
-    
+
+    def upgradeDb
+      # FIXME: Support upgrading the DB
+    end
+
     ###
     # Write message to torrentprocessor log
     #
@@ -307,12 +310,12 @@ module TorrentProcessor
       $LOG.debug "Controller::log( msg )"
       logfile = File.join( @cfg[:logdir], @logfile )
       timestamp = DateTime.now.strftime()
-      
+
       File.open( logfile, 'a' ) {|f| f.write( "#{timestamp}:  #{msg}\n" ); f.flush; }
-                
+
     end
-      
-    
+
+
     ###
     # Rotate torrentprocessor logs
     #
@@ -320,19 +323,19 @@ module TorrentProcessor
       $LOG.debug "Controller::rotate_logs()"
       max_size = @cfg[:maxlogsize]
       return if max_size == 0
-      
+
       logfile = File.join( @cfg[:logdir], @logfile )
-      
+
       if (File.new( logfile ).size > max_size)
         FileUtils.rm( "#{logfile}.3" ) if File.exists?( "#{logfile}.3" )
         FileUtils.mv( "#{logfile}.2", "#{logfile}.3" ) if File.exists?( "#{logfile}.2" )
         FileUtils.mv( "#{logfile}.1", "#{logfile}.2" ) if File.exists?( "#{logfile}.1" )
         FileUtils.mv( "#{logfile}", "#{logfile}.1" ) if File.exists?( "#{logfile}" )
       end
-                
+
     end
-      
-    
+
+
     ###
     # Add a tracker seedlimit filter to the config file
     #
@@ -346,7 +349,7 @@ module TorrentProcessor
       c = Config.new
       c.cfg = @cfg
       c.save
-      
+
     end
 
 
@@ -376,7 +379,7 @@ module TorrentProcessor
       c = Config.new
       c.cfg = @cfg
       c.save
-      
+
     end
 
 
@@ -390,7 +393,7 @@ module TorrentProcessor
       c = Config.new
       c.cfg = @cfg
       c.save
-      
+
     end
 
 
@@ -404,7 +407,7 @@ module TorrentProcessor
       c = Config.new
       c.cfg = @cfg
       c.save
-      
+
     end
 
 
@@ -418,7 +421,7 @@ module TorrentProcessor
       c = Config.new
       c.cfg = @cfg
       c.save
-      
+
     end
 
 
