99947480e2d44918c5a1fdf11c6b4342baeb1641
Jeff McAffee <jeff@ktechsystems.com>
2013-09-18 21:52:42 -0700

    Replace tabs with spaces - remove trailing whitespace
    	modified:   bin/torrentprocessor
    	modified:   rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -27,8 +27,8 @@ DISTDIR         = "./dist"
 $:.unshift File.expand_path("../lib", __FILE__)
 require "torrentprocessor/version"
 
-PKG_VERSION	= TorrentProcessor::VERSION
-PKG_FILES 	= Dir["**/*"].select { |d| d =~ %r{^(README|bin/|data/|ext/|lib/|spec/|test/)} }
+PKG_VERSION = TorrentProcessor::VERSION
+PKG_FILES   = Dir["**/*"].select { |d| d =~ %r{^(README|bin/|data/|ext/|lib/|spec/|test/)} }
 
 # Setup common clean and clobber targets
 
@@ -51,8 +51,8 @@ directory DISTDIR
 # Load local tasks.
 imports = FileList['tasks/**/*.rake']
 imports.each do |imp|
-	puts "== Importing local task file: #{imp}" if $verbose
-	import "#{imp}"
+  puts "== Importing local task file: #{imp}" if $verbose
+  import "#{imp}"
 end
 
 
@@ -67,45 +67,45 @@ end
 #############################################################################
 desc "Build a OCRA executable"
 task :exe => [:init] do
-	if (!File.exists?("#{BUILDDIR}/#{PROJNAME}.exe"))
-			puts "*** Generating executable #{PROJNAME}.exe"
-			REAL_LOCATION = File.absolute_path(".")
-			puts "REAL_LOCATION: #{REAL_LOCATION}"
-			cp("./bin/#{PROJNAME}", "#{BUILDDIR}/#{PROJNAME}.rb")
-			cd("#{REAL_LOCATION}") do |d|
-				# Using --no-lzma will turn off lzma compression. This may reduce application start up time:
-				#		From http://rubyforge.org/pipermail/wxruby-users/2009-September.txt :
-				#			Try building the executable with the --no-lzma option. The resulting 
-				#			file will be bigger but it may well start faster. LZMA is a very 
-				# 		efficient compression algorithm but quite slow.
+  if (!File.exists?("#{BUILDDIR}/#{PROJNAME}.exe"))
+      puts "*** Generating executable #{PROJNAME}.exe"
+      REAL_LOCATION = File.absolute_path(".")
+      puts "REAL_LOCATION: #{REAL_LOCATION}"
+      cp("./bin/#{PROJNAME}", "#{BUILDDIR}/#{PROJNAME}.rb")
+      cd("#{REAL_LOCATION}") do |d|
+        # Using --no-lzma will turn off lzma compression. This may reduce application start up time:
+        #   From http://rubyforge.org/pipermail/wxruby-users/2009-September.txt :
+        #     Try building the executable with the --no-lzma option. The resulting 
+        #     file will be bigger but it may well start faster. LZMA is a very 
+        #     efficient compression algorithm but quite slow.
 
-				# TODO: Test with no lzma and see if the start up time improves.
+        # TODO: Test with no lzma and see if the start up time improves.
 
-				output = `ocra --console #{BUILDDIR}/#{PROJNAME}.rb --no-lzma --gemfile gemfile`
-				puts output
-			end
-	end
-	
-	mv("./#{PROJNAME}.exe", "#{BUILDDIR}/#{PROJNAME}.exe")
+        output = `ocra --console #{BUILDDIR}/#{PROJNAME}.rb --no-lzma --gemfile gemfile`
+        puts output
+      end
+  end
+  
+  mv("./#{PROJNAME}.exe", "#{BUILDDIR}/#{PROJNAME}.exe")
 end
 
 
 #############################################################################
 desc "Documentation for building gem and executable"
 task :help do
-	hr = "-"*79
-	puts hr
-	puts "Building the Gem and Ocra Executable"
-	puts "===================================="
-	puts
-	puts "Use the following command line to build and install the gem, then"
-	puts "build the executable (by letting Ocra run the gem)."
-	puts 
-	puts "rake clean gem && gem install pkg\\torrentprocessor-#{PKG_VERSION}.gem -l --no-ri --no-rdoc && rake exe"
-	puts
-	puts "The executable will be located in the build dir when finished."
-	puts
-	puts hr
+  hr = "-"*79
+  puts hr
+  puts "Building the Gem and Ocra Executable"
+  puts "===================================="
+  puts
+  puts "Use the following command line to build and install the gem, then"
+  puts "build the executable (by letting Ocra run the gem)."
+  puts 
+  puts "rake clean gem && gem install pkg\\torrentprocessor-#{PKG_VERSION}.gem -l --no-ri --no-rdoc && rake exe"
+  puts
+  puts "The executable will be located in the build dir when finished."
+  puts
+  puts hr
 end
 
 
@@ -113,8 +113,8 @@ end
 RDoc::Task.new(:rdoc) do |rdoc|
     files = ['docs/**/*.rdoc', 'lib/**/*.rb', 'app/**/*.rb']
     rdoc.rdoc_files.add( files )
-    rdoc.main = "docs/README.rdoc"           	# Page to start on
-	#puts "PWD: #{FileUtils.pwd}"
+    rdoc.main = "docs/README.rdoc"            # Page to start on
+  #puts "PWD: #{FileUtils.pwd}"
     rdoc.title = "#{PROJNAME} Documentation"
     rdoc.rdoc_dir = 'doc'                   # rdoc output folder
     rdoc.options << '--line-numbers' << '--all'
@@ -132,20 +132,20 @@ end
 
 #############################################################################
 SPEC = Gem::Specification.new do |s|
-	s.platform = Gem::Platform::RUBY
-	s.summary = "Process torrent files"
-	s.name = PROJNAME.downcase
-	s.version = PKG_VERSION
-	s.requirements << 'none'
-	s.bindir = 'bin'
-	s.require_path = 'lib'
-	#s.autorequire = 'rake'
-	s.files = PKG_FILES
-	s.executables = "torrentprocessor"
-	s.author = "Jeff McAffee"
-	s.email = "gems@ktechdesign.com"
-	s.homepage = "http://gems.ktechdesign.com"
-	s.description = <<EOF
+  s.platform = Gem::Platform::RUBY
+  s.summary = "Process torrent files"
+  s.name = PROJNAME.downcase
+  s.version = PKG_VERSION
+  s.requirements << 'none'
+  s.bindir = 'bin'
+  s.require_path = 'lib'
+  #s.autorequire = 'rake'
+  s.files = PKG_FILES
+  s.executables = "torrentprocessor"
+  s.author = "Jeff McAffee"
+  s.email = "gems@ktechdesign.com"
+  s.homepage = "http://gems.ktechdesign.com"
+  s.description = <<EOF
 TorrentProcessor will process torrent downloads to copy, move and delete
 torrents throughout the torrent processing lifecycle.
 EOF
@@ -155,19 +155,19 @@ end
 #############################################################################
 desc "Run all tests"
 task :test => [:init] do
-	unless File.directory?('test')
-		$stderr.puts 'no test in this package'
-		return
-	end
-	$stderr.puts 'Running tests...'
-	begin
-		require 'test/unit'
-	rescue LoadError
-		$stderr.puts 'test/unit cannot loaded.  You need Ruby 1.8 or later to invoke this task.'
-	end
-	
-	$LOAD_PATH.unshift("./")
-	$LOAD_PATH.unshift(TESTDIR)
-	Dir[File.join(TESTDIR, "*.rb")].each {|file| require File.basename(file) }
-	require 'minitest/autorun'
+  unless File.directory?('test')
+    $stderr.puts 'no test in this package'
+    return
+  end
+  $stderr.puts 'Running tests...'
+  begin
+    require 'test/unit'
+  rescue LoadError
+    $stderr.puts 'test/unit cannot loaded.  You need Ruby 1.8 or later to invoke this task.'
+  end
+  
+  $LOAD_PATH.unshift("./")
+  $LOAD_PATH.unshift(TESTDIR)
+  Dir[File.join(TESTDIR, "*.rb")].each {|file| require File.basename(file) }
+  require 'minitest/autorun'
 end
