0bc8e7f3aea0aec34abf7cff5780ebf123b6f907
Jeff McAffee <jeff@ktechsystems.com>
2013-10-19 12:13:24 -0700

    Move rssdata.rb to rss_torrent_data.rb
    	renamed:    lib/torrentprocessor/rssdata.rb -> lib/torrentprocessor/rss_torrent_data.rb
    	modified:   lib/torrentprocessor/utorrentwebui.rb

--- lib/torrentprocessor/utorrentwebui.rb
+++ lib/torrentprocessor/utorrentwebui.rb
@@ -1,7 +1,7 @@
 ##############################################################################
 # File::    utorrentwebui.rb
 # Purpose:: Web UI object for uTorrent.
-# 
+#
 # Author::    Jeff McAffee 07/31/2011
 # Copyright:: Copyright (c) 2011, kTech Systems LLC. All rights reserved.
 # Website::   http://ktechsystems.com
@@ -13,11 +13,11 @@ require 'json/pure'
 require 'net/http'
 require 'hpricot'
 require 'torrentdata'
-require "rssdata"
+require "rss_torrent_data"
 
 
 module TorrentProcessor
-    
+
   ##########################################################################
   # UTorrentWebUI class
   class UTorrentWebUI
@@ -29,13 +29,13 @@ module TorrentProcessor
     attr_reader   :rssfeeds
     attr_reader   :rssfilters
     attr_reader   :settings
-    
+
     ###
     # Processor constructor
     #
     def initialize(ip, port, user, pass)
       $LOG.debug "UTorrentWebUI::initialize( ip:#{ip}, port:#{port} user:#{user} pass:#{pass}"
-      
+
       @ip         = ip
       @port       = port
       @user       = user
@@ -48,14 +48,14 @@ module TorrentProcessor
       @verbose    = false
       @settings   = Array.new
       @torrentc   = nil
-      
+
       @torrents         = Hash.new
       @torrents_removed = Hash.new
       @rssfeeds         = Hash.new
       @rssfilters       = Hash.new
-      
+
     end
-    
+
 
     ###
     # Start a HTTP session
@@ -66,8 +66,8 @@ module TorrentProcessor
       $LOG.debug "UTorrentWebUI::startSession()"
       @http = Net::HTTP.start(@ip, @port)
     end
-    
-    
+
+
     ###
     # Stop a HTTP session
     #
@@ -75,8 +75,8 @@ module TorrentProcessor
       $LOG.debug "UTorrentWebUI::stopSession()"
       @http.finish
     end
-    
-    
+
+
     ###
     # Send a GET query
     #
@@ -87,7 +87,7 @@ module TorrentProcessor
       req = Net::HTTP::Get.new(query)
       req.basic_auth @user, @pass
       req["cookie"] = @cookie if @cookie
-      
+
       if @verbose
         puts "  REQUEST HEADERS:"
         req.each_header do |k,v|
@@ -95,9 +95,9 @@ module TorrentProcessor
         end
         puts
       end # if @verbose
-      
+
       @response = @http.request(req)
-      
+
       if @verbose
         puts "  RESPONSE HEADERS:"
         @response.each_header do |k,v|
@@ -108,12 +108,12 @@ module TorrentProcessor
 
       data = @response.body
       raise "Invalid response. Check the address, login and password of the server." if data.nil? || data.empty?
-      
+
       $LOG.debug "  Response Body: #{data}"
       data
     end
-    
-    
+
+
     ###
     # Get the uTorrent token for queries
     #
@@ -124,12 +124,12 @@ module TorrentProcessor
       data = Hpricot(@response.body)
       @token = data.at("div[#token]").inner_html
       $LOG.debug "  Token: #{@token}"
-      
+
       storeCookie()
       @token
     end
-    
-    
+
+
     ###
     # Store the cookie if sent
     #
@@ -137,10 +137,10 @@ module TorrentProcessor
       tmpcookie = @response["set-cookie"]
       @cookie = tmpcookie.split(";")[0] if !tmpcookie.nil?
       $LOG.debug "  Cookie set: #{@cookie}" if !tmpcookie.nil?
-      
+
     end
-    
-    
+
+
     ###
     # Send a GET query
     #
@@ -148,18 +148,18 @@ module TorrentProcessor
     #
     def sendGetQuery(query)
       $LOG.debug "UTorrentWebUI::sendQuery( #{query} )"
-    
+
       data = nil
       startSession()
       getToken()
       data = getQuery("#{query}&token=#{@token}")
       stopSession()
-      
+
       return data
-      
+
     end
-    
-    
+
+
     ###
     # Parse the response data (using JSON)
     #
@@ -169,27 +169,27 @@ module TorrentProcessor
         $LOG.debug "Response is NIL or empty."
         return
       end
-      
+
       result = JSON.parse(@response.body)
     end
-    
-    
+
+
     ###
     # Get uTorrent settings
     #
     def get_utorrent_settings()
       $LOG.debug "UTorrentWebUI::get_utorrent_settings()"
-    
+
       @url = "/gui/?action=getsettings"
-      
+
       sendGetQuery(@url)
       result = parseResponse()
-      
+
       @settings = result["settings"]
-      
+
     end
-    
-    
+
+
     ###
     # Get a torrent's job properties
     #
@@ -235,31 +235,31 @@ module TorrentProcessor
     #
     def remove_torrent(hash)
       $LOG.debug "UTorrentWebUI::remove_torrent( hash )"
-    
+
       @url = "/gui/?action=removedata&hash=#{hash}"
-      
+
       sendGetQuery(@url)
       result = parseResponse()
     end
-    
-    
+
+
     ###
     # Get a list of Torrents
     #
     def get_torrent_list(cache_id = nil)
       $LOG.debug "UTorrentWebUI::get_torrent_list( #{cache_id} )"
-      
+
       @url = "/gui/?list=1"
       @url = "/gui/?list=1&cache=#{cache_id}" if !cache_id.nil?
-      
+
       sendGetQuery(@url)
       result = parseResponse()
-      
+
       return parseListRequestResponse( result )
-      
+
     end
-      
-    
+
+
     ###
     # Get a list of Torrents using a cache value
     #
@@ -267,15 +267,15 @@ module TorrentProcessor
       $LOG.debug "UTorrentWebUI::get_torrent_list_using_cache( #{cache_id} )"
       # TODO: Remove this method
       @url = "/gui/?list=1&cache=#{cache_id}"
-      
+
       sendGetQuery(@url)
       result = parseResponse()
-      
+
       return parseListRequestResponse( result )
-      
+
     end
-      
-    
+
+
     ###
     # Parse a response result from a torrent list request
     #
@@ -283,27 +283,27 @@ module TorrentProcessor
     #
     def parseListRequestResponse(response)
       $LOG.debug "UTorrentWebUI::parseListRequestResponse(response)"
-    
+
       # Clear out the torrents hash
       @torrents.clear unless @torrents.nil?
-      
+
       # Clear out the removed torrents hash
       @torrents_removed.clear unless @torrents_removed.nil?
-      
+
       # Clear out the RSS Feeds hash
       @rssfeeds.clear unless @rssfeeds.nil?
-      
+
       # Clear out the RSS Filters hash
       @rssfilters.clear unless @rssfilters.nil?
-      
+
       # Stash the cache
       @torrentc = response["torrentc"]
       $LOG.info "    Cache value stored: #{@torrentc}"
-      
+
       parseTorrentListResponse( response )      if response.include?("torrents")
       parseTorrentListCacheResponse( response ) if response.include?("torrentsp")
       $LOG.error("  List Request Response does not contain either 'torrents' or 'torrentsp'") if (!response.include?("torrents") && !response.include?("torrentsp"))
-      
+
       parseRssFeedsListResponse( response )     if response.include?("rssfeeds")
       $LOG.info("  List Request Response does not contain RSS Feed data") if (!response.include?("rssfeeds"))
 
@@ -312,8 +312,8 @@ module TorrentProcessor
 
       return response
     end
-    
-    
+
+
     ###
     # Parse a response result from a torrent list request
     #
@@ -322,16 +322,16 @@ module TorrentProcessor
     def parseTorrentListResponse(response)
       $LOG.debug "UTorrentWebUI::parseTorrentListResponse(response)"
       torrents = response["torrents"]
-      
+
       # torrents is an array of arrays
       torrents.each do |t|
         td = TorrentData.new(t)
         @torrents[td.hash] = td
       end
-          
+
     end
-    
-    
+
+
     ###
     # Parse a response result from a torrent list cache request
     #
@@ -340,23 +340,23 @@ module TorrentProcessor
     def parseTorrentListCacheResponse(response)
       $LOG.debug "UTorrentWebUI::parseTorrentListCacheResponse(response)"
       torrents = response["torrentsp"]
-      
+
       # torrents is an array of arrays
       torrents.each do |t|
         td = TorrentData.new(t)
         @torrents[td.hash] = td
       end
-      
+
       # Store the 'removed' torrents
       removed = response["torrentsm"]
       removed.each do |t|
         td = TorrentData.new(t)
         @torrents_removed[td.hash] = td
       end
-    
+
     end
-    
-    
+
+
     ###
     # Parse a rssfeed response result from a torrent list request
     #
@@ -365,16 +365,16 @@ module TorrentProcessor
     def parseRssFeedsListResponse(response)
       $LOG.debug "UTorrentWebUI::parseRssFeedsListResponse(response)"
       feeds = response["rssfeeds"]
-      
+
       # feeds is an array of arrays
       feeds.each do |f|
         feed = RSSFeed.new(f)
         @rssfeeds[feed.feed_name] = feed
       end
-          
+
     end
-    
-    
+
+
     ###
     # Parse a rssfilter response result from a torrent list request
     #
@@ -383,16 +383,16 @@ module TorrentProcessor
     def parseRssFiltersListResponse(response)
       $LOG.debug "UTorrentWebUI::parseRssFiltersListResponse(response)"
       filters = response["rssfilters"]
-      
+
       # filters is an array of arrays
       filters.each do |f|
         filter = RSSFilter.new(f)
         @rssfilters[filter.feed_name] = filter
       end
-          
+
     end
-    
-    
+
+
     ###
     # Set the verbose flag
     #
@@ -402,7 +402,7 @@ module TorrentProcessor
       $LOG.debug "UTorrentWebUI::verbose=( #{arg} )"
       @verbose = arg
     end
-     
+
 
     ###
     # Indicates if there are torrents that have been removed.
@@ -414,18 +414,18 @@ module TorrentProcessor
       return false if (@removed_torrents.nil? || @removed_torrents.length == 0)
       return true
     end
-     
+
 
     ###
     # Return the cache token
     #
     def cache()
       $LOG.debug "UTorrentWebUI::cache()"
-    
+
       return @torrentc
     end
-    
-    
+
+
   end # class UTorrentWebUI
 
 
