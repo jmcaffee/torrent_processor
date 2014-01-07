0e6276b77c13072aa9d62d527cdaf52af23fdff4
Jeff McAffee <jeff@ktechsystems.com>
2014-01-06 23:01:38 -0700

    Add support for RAR extraction using 7Zip
    	new file:   lib/torrentprocessor/processor_plugin/seven_zip.rb
    	new file:   spec/data/multi_rar/test_250kb.part01.rar
    	new file:   spec/data/multi_rar/test_250kb.part02.rar
    	new file:   spec/data/multi_rar/test_250kb.part03.rar
    	new file:   spec/data/multi_rar/test_250kb.part04.rar
    	new file:   spec/data/multi_rar/test_250kb.part05.rar
    	new file:   spec/data/multi_rar/test_250kb.part06.rar
    	new file:   spec/data/old_style_rar/test_250kb.r00
    	new file:   spec/data/old_style_rar/test_250kb.r01
    	new file:   spec/data/old_style_rar/test_250kb.r02
    	new file:   spec/data/old_style_rar/test_250kb.r03
    	new file:   spec/data/old_style_rar/test_250kb.r04
    	new file:   spec/data/old_style_rar/test_250kb.rar
    	new file:   spec/data/rar_source/test_250kb.avi
    	new file:   spec/seven_zip_spec.rb

---
+++ lib/torrentprocessor/processor_plugin/seven_zip.rb
@@ -0,0 +1,111 @@
+############################################################################
+# File::    seven_zip.rb
+# Purpose:: Interface to windows 7Zip utility
+#
+# Author::    Jeff McAffee 2014-01-05
+# Copyright:: Copyright (c) 2014, kTech Systems LLC. All rights reserved.
+# Website::   http://ktechsystems.com
+##############################################################################
+
+# 7-Zip Help
+#
+#
+# 7-Zip 9.20  Copyright (c) 1999-2010 Igor Pavlov  2010-11-18
+#
+# Usage: 7z <command> [<switches>...] <archive_name> [<file_names>...]
+#        [<@listfiles...>]
+#
+# <Commands>
+#   a: Add files to archive
+#   b: Benchmark
+#   d: Delete files from archive
+#   e: Extract files from archive (without using directory names)
+#   l: List contents of archive
+#   t: Test integrity of archive
+#   u: Update files to archive
+#   x: eXtract files with full paths
+# <Switches>
+#   -ai[r[-|0]]{@listfile|!wildcard}: Include archives
+#   -ax[r[-|0]]{@listfile|!wildcard}: eXclude archives
+#   -bd: Disable percentage indicator
+#   -i[r[-|0]]{@listfile|!wildcard}: Include filenames
+#   -m{Parameters}: set compression Method
+#   -o{Directory}: set Output directory
+#   -p{Password}: set Password
+#   -r[-|0]: Recurse subdirectories
+#   -scs{UTF-8 | WIN | DOS}: set charset for list files
+#   -sfx[{name}]: Create SFX archive
+#   -si[{name}]: read data from stdin
+#   -slt: show technical information for l (List) command
+#   -so: write data to stdout
+#   -ssc[-]: set sensitive case mode
+#   -ssw: compress shared files
+#   -t{Type}: Set type of archive
+#   -u[-][p#][q#][r#][x#][y#][z#][!newArchiveName]: Update options
+#   -v{Size}[b|k|m|g]: Create volumes
+#   -w[{path}]: assign Work directory. Empty path means a temporary directory
+#   -x[r[-|0]]]{@listfile|!wildcard}: eXclude filenames
+#   -y: assume Yes on all queries
+#
+#
+##############################################################################
+
+
+module TorrentProcessor::ProcessorPlugin
+
+  class SevenZip
+
+    def SevenZip.app_path=(path)
+      @@app_path = path
+    end
+
+    def SevenZip.app_path
+      return @@app_path unless (!defined?(@@app_path) || @@app_path.nil?)
+
+      possible_locations = ['C:/Program Files/7Zip/7z.exe',
+                            'C:/Program Files (x86)/7Zip/7z.exe',
+                            'C:/opt/bin/7Zip/7z.exe']
+
+      possible_locations.each do |path|
+        if File.exists?(path)
+          @@app_path = path
+          return @@app_path
+        end
+      end
+      raise 'SevenZip Error: Unable to find 7Zip executable.'
+    end
+
+    def SevenZip.default_commands
+      commands =  'x'           # Extract files with full paths
+    end
+
+    def SevenZip.default_switches
+      switches =  '-y'          # Assume Yes on all queries
+    end
+
+    def SevenZip.extract_rar(src_dir, out_dir, logger = nil)
+      switches = SevenZip.default_switches
+      switches << " -o#{out_dir}"
+
+      cmd_line = "#{SevenZip.default_commands} #{switches} #{SevenZip.quote(src_dir)}"
+      app_cmd = "#{app_path} #{cmd_line}"
+      logger.log "Executing: #{app_cmd}" unless logger.nil?
+
+      result = Kernel.system("#{app_cmd}")
+      if result
+          logger.log ("    ERROR: #{app_path} failed. Command line it was called with: ".concat(app_cmd) ) unless logger.nil?
+          return false
+      end
+      true
+    end
+
+    ###
+    # Quote a string
+    #
+    # str:: String to apply quotes to
+    #
+    def SevenZip.quote(str)
+      return "\"#{str}\""
+    end
+  end # class SevenZip
+end # module TorrentProcessor::ProcessorPlugin
---
+++ spec/seven_zip_spec.rb
@@ -0,0 +1,96 @@
+##############################################################################
+# File::    seven_zip_spec.rb
+# Purpose:: SevenZip specifications
+# 
+# Author::    Jeff McAffee 01/05/2014
+# Copyright:: Copyright (c) 2014, kTech Systems LLC. All rights reserved.
+# Website::   http://ktechsystems.com
+##############################################################################
+
+require_relative './spec_helper'
+
+include FileUtils
+include TorrentProcessor::ProcessorPlugin
+
+describe SevenZip do
+
+  context ".app_path" do
+
+    it "returns absolute path to 7z.exe" do
+      SevenZip.app_path
+    end
+  end
+
+  context ".app_path=" do
+
+    it "overrides application path to executable" do
+      SevenZip.app_path = 'test'
+      expect(SevenZip.app_path).to eq 'test'
+      # Reset to nil so we don't break other tests.
+      SevenZip.app_path = nil
+    end
+  end
+
+  context ".default_commands" do
+
+    it "defaults to extract command" do
+      expect(SevenZip.default_commands).to eq 'x'
+    end
+  end
+
+  context ".default_switches" do
+
+    it "defaults to 'Yes for all' switch" do
+      expect(SevenZip.default_switches).to eq '-y'
+    end
+  end
+
+  context ".extract_rar" do
+
+    let(:data_dir)      { 'spec/data' }
+    let(:multi_rar)     { 'multi_rar' }
+    let(:old_style_rar) { 'old_style_rar' }
+    let(:tmp_dir) do
+      mkdir_p 'tmp/spec/seven_zip'
+      'tmp/spec/seven_zip'
+    end
+
+    context "current rar naming convention (*.part01.rar, *.part02.rar)" do
+
+      context "given a directory path" do
+        let(:test_dir) do
+          td = File.join(tmp_dir, multi_rar)
+          blocking_dir_delete td
+          cp_r(File.join(data_dir, multi_rar), tmp_dir)
+          td
+        end
+        let(:extracted_rar) { File.join(test_dir, 'test_250kb.avi') }
+
+        it "finds and extracts a rar archive" do
+          SevenZip.extract_rar(test_dir, test_dir, nil)
+          expect(File.exists?(extracted_rar)).to eq true
+        end
+      end
+    end
+
+    context "old style rar naming convention (*.r00, *.r01, *.rar)" do
+
+      context "given a directory path" do
+        let(:test_dir) do
+          td = File.join(tmp_dir, old_style_rar)
+          blocking_dir_delete td
+          cp_r(File.join(data_dir, old_style_rar), tmp_dir)
+          td
+        end
+        let(:extracted_rar) { File.join(test_dir, 'test_250kb.avi') }
+
+        it "finds and extracts a rar archive" do
+          #SevenZip.extract_rar(test_dir, test_dir, SimpleLogger)
+          SevenZip.extract_rar(test_dir, test_dir, nil)
+          expect(File.exists?(extracted_rar)).to eq true
+        end
+      end
+    end
+  end
+end
+
