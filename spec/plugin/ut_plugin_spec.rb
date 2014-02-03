efa4f0c0b00a2d9bfc3c6cda3d21dd387e3b1aa2
Jeff McAffee <jeff@ktechsystems.com>
2014-02-03 14:49:30 -0700

    Remove unused stubs

--- spec/plugin/ut_plugin_spec.rb
+++ spec/plugin/ut_plugin_spec.rb
@@ -18,44 +18,10 @@ describe UTPlugin do
 
   let(:tmp_path) { 'tmp/spec/ut_plugin' }
 
-  let(:controller_stub) do
-    obj = double('controller')
-    obj.stub(:cfg) { cfg_stub }
-    obj.stub(:database) { db_stub }
-    obj
-  end
-
   let(:db_stub) do
     obj = double('database')
     obj.stub(:close) { true }
     obj
-  end
-
-  let(:cfg_stub) do
-    cfg = TorrentProcessor.configuration
-    #tmp_path = File.absolute_path(File.join(File.dirname(__FILE__), '../../tmp/spec/tpsetup'))
-    cfg.app_path          = tmp_path
-    cfg.logging           = false
-    cfg.max_log_size      = 0
-    cfg.log_dir           = tmp_path
-    cfg.tv_processing     = File.join(tmp_path, 'media/tv')
-    cfg.movie_processing  = File.join(tmp_path, 'media/movies')
-    cfg.other_processing  = File.join(tmp_path, 'media/other')
-    cfg.filters           = {}
-
-    cfg.utorrent.ip                     = '192.168.1.103'
-    cfg.utorrent.port                   = '8082'
-    cfg.utorrent.user                   = 'admin'
-    cfg.utorrent.pass                   = 'abc'
-    cfg.utorrent.dir_completed_download = File.join(tmp_path, 'torrents/completed')
-    cfg.utorrent.seed_ratio             = 0
-
-    cfg.tmdb.api_key              = '***REMOVED***'
-    cfg.tmdb.language             = 'en'
-    cfg.tmdb.target_movies_path   = File.join(tmp_path, 'movies_final')
-    cfg.tmdb.can_copy_start_time  = "00:00"
-    cfg.tmdb.can_copy_stop_time   = "23:59"
-    cfg
   end
 
   let(:ut_stub) do
