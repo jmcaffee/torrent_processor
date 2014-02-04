ef86293850a6e40ffa19627a9070a16c53ee5fba
Jeff McAffee <jeff@ktechsystems.com>
2014-02-04 14:53:18 -0700

    Add service module require

---
+++ lib/torrentprocessor/service.rb
@@ -0,0 +1,18 @@
+##############################################################################
+# File::    service.rb
+# Purpose:: Service module/classes
+#
+# Author::    Jeff McAffee 2014-02-04
+# Copyright:: Copyright (c) 2014, kTech Systems LLC. All rights reserved.
+# Website::   http://ktechsystems.com
+##############################################################################
+
+##########################################################################
+# TorrentProcessor module
+module TorrentProcessor::Service
+end # module TorrentProcessor::Service
+
+require_relative('service/utorrent')
+require_relative('service/robocopy')
+require_relative('service/seven_zip')
+
