463ab5ee8f7f12c8d7f886d3bd518a507be7665a
Jeff McAffee <jeff@ktechsystems.com>
2014-02-04 21:04:56 -0700

    Remove $LOG initialization

--- lib/torrentprocessor.rb
+++ lib/torrentprocessor.rb
@@ -87,44 +87,6 @@ TorrentProcessor.configure do |config|
 end
 
 ##############################################################################
-# Logging
-#
-if(!$LOG)
-  $LOG = Logger.new(STDERR)
-  $LOG.level = Logger::ERROR
-end
-
-# $LOGGING should be false when releasing a production build.
-# Turning on logging will also result in stack traces being displayed when
-# exceptions are thrown.
-
-$LOGGING = false
-#$LOGGING = true           # Uncomment this line to force logging
-
-
-#require "#{File.join( File.dirname(__FILE__), 'torrentprocessor','config')}"
-#  logcfg = TorrentProcessor::Config.new.load
-#  if(logcfg.key?(:logging) && (true == logcfg[:logging]) )
-#    $LOGGING = true
-#  end
-#
-#  if($LOGGING)
-#    # Create a new log file each time:
-#    file = File.open('torrentprocessor.log', File::WRONLY | File::APPEND | File::CREAT | File::TRUNC)
-#    $LOG = Logger.new(file)
-#    $LOG.level = Logger::DEBUG
-#    #$LOG.level = Logger::INFO
-#  else
-#    if(File.exists?('torrentprocessor.log'))
-#      FileUtils.rm('torrentprocessor.log')
-#    end
-#  end
-#  $LOG.info "**********************************************************************"
-#  $LOG.info "Logging started for TorrentProcessor library."
-#  $LOG.info "**********************************************************************"
-
-
-##############################################################################
 # Require each lib file
 #
 class_files = File.join( File.dirname(__FILE__), 'torrentprocessor', '*.rb')
