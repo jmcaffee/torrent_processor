25e15d01bdc4492ae3aa1833ec348a2c6087bf93
Jeff McAffee <jeff@ktechsystems.com>
2015-01-31 11:02:28 -0700

    Add mixin requires
    	modified:   lib/torrentprocessor/utility.rb

--- lib/torrentprocessor/utility.rb
+++ lib/torrentprocessor/utility.rb
@@ -2,4 +2,9 @@ module TorrentProcessor::Utility; end
 
 require_relative 'utility/dir_helper'
 require_relative 'utility/formatter'
+require_relative 'utility/loggers'
+
+# Mixins
+require_relative 'utility/loggable'
+require_relative 'utility/verbosable'
 
