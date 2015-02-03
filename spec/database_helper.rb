d906cc922813aa8ae8ef35a5966422ccfb61af88
Jeff McAffee <jeff@ktechsystems.com>
2015-02-03 10:09:07 -0700

    Add stub for :read
    	modified:   spec/database_helper.rb

--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -30,6 +30,31 @@ module DatabaseHelper
         []
       end
     end
+    obj.stub(:read) do |q|
+      if q.include? 'SELECT hash FROM torrents'
+        [
+          [ 'abc' ]
+        ]
+      elsif q.include? 'SELECT hash, name FROM torrents WHERE (tp_state IS NULL AND id = 2)'
+        [
+          [ 'abc', 'TestTorrent1' ]
+        ]
+      elsif q.include? 'SELECT id,ratio,name from torrents'
+        [
+          [ 2, 1500, 'TestTorrent1' ]
+        ]
+      elsif q.include? 'SELECT id,tp_state,name from torrents'
+        [
+          [ 2, 'removing', 'TestTorrent1' ]
+        ]
+      elsif q.include? "SELECT name from sqlite_master WHERE type = 'table' ORDER BY name"
+        [
+          [ 'torrents' ]
+        ]
+      else
+        []
+      end
+    end
 
     obj.stub(:delete_torrent) do |arg|
     end
