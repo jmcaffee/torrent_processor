991b80d27a321f5f563e92ad110cc1234b8a7734
Jeff McAffee <jeff@ktechsystems.com>
2015-02-19 13:17:23 -0700

    Turn on mock verifications

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,19 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
   config.order = 59885
+
+  config.mock_with :rspec do |mocks|
+
+    # This option should be set when all dependencies are being loaded
+    # before a spec run, as is the case in a typical spec helper. It will
+    # cause any verifying double instantiation for a class that does not
+    # exist to raise, protecting against incorrectly spelt names.
+    mocks.verify_doubled_constant_names = true
+
+    # Force method verification of partial doubles too
+    mocks.verify_partial_doubles = true
+
+  end
 end
 
 include FileUtils
