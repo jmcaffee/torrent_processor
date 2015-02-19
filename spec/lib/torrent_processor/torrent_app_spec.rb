4ac7a85cd975887ba81a3d2efd8695280e2cbf76
Jeff McAffee <jeff@ktechsystems.com>
2015-02-19 13:18:44 -0700

    Refactor TorrentApp spec to just test pass-though to adapter

--- spec/lib/torrent_processor/torrent_app_spec.rb
+++ spec/lib/torrent_processor/torrent_app_spec.rb
@@ -4,15 +4,6 @@ include TorrentProcessor
 
 describe TorrentApp do
 
-  let(:utorrent_stub) { Mocks.utorrent }
-
-  let(:app) {
-    TorrentApp.new(:cfg         => Mocks.cfg('torrent_app'),
-                   :webui       => utorrent_stub,
-                   :webui_type  => :utorrent,
-                   :database    => Mocks.db)
-  }
-
   context "#new" do
 
     it "can be instantiated" do
@@ -25,7 +16,7 @@ describe TorrentApp do
     end
 
     it ":webui_type must be provided if passing :webui" do
-      expect{ TorrentApp.new(:webui => utorrent_stub) }.to raise_exception
+      expect{ TorrentApp.new(:webui => Mocks.utorrent) }.to raise_exception
     end
 
     it "unrecognized :webui_type raises an exception" do
@@ -34,104 +25,90 @@ describe TorrentApp do
     end
   end
 
-  context "#seed_ratio" do
+  let(:backend_mock) { Mocks.qbtorrent }
 
-    it "returns the global seed_ratio" do
-      expect(utorrent_stub).to receive(:get_utorrent_settings)
-      app.seed_ratio.class
-    end
+  let(:init_args) do
+    {
+      :cfg        => Mocks.cfg('torrent_app'),
+      :webui      => backend_mock,
+      :webui_type => :qbtorrent,
+      :database   => Mocks.db,
+    }
   end
 
-  context "#completed_downloads_dir" do
+  let(:adapter_mock) { Mocks.qbt_adapter(init_args) }
 
-    it "returns the completed downloads directory" do
-      expect(utorrent_stub).to receive(:get_utorrent_settings)
+  let(:app) {
+    app_args = init_args
+    app_args[:adapter] = adapter_mock
+
+    TorrentApp.new(app_args)
+  }
+
+  context "calls adapter methods" do
+
+    it "#seed_ratio" do
+      expect(adapter_mock).to receive(:seed_ratio)
+      app.seed_ratio
+    end
+
+    it "#completed_downloads_dir" do
+      expect(adapter_mock).to receive(:completed_downloads_dir)
       dir = app.completed_downloads_dir
     end
-  end
 
-  context "#app_name" do
-
-    it "returns the name of the configured app" do
-      expect(app.app_name).to eq 'uTorrent'
+    it "#app_name" do
+      expect(app.app_name).to eq 'qBitTorrent'
     end
-  end
 
-  context "#torrent_list" do
-
-    it "returns a list of torrents" do
-      expect(utorrent_stub).to receive(:get_torrent_list).with(any_args)
+    it "#torrent_list" do
+      expect(adapter_mock).to receive(:torrent_list).with(any_args)
       app.torrent_list
     end
-  end
 
-  context "#get_torrent_job_properties" do
-
-    it "returns properties of a torrent" do
+    it "#get_torrent_job_properties" do
       hash = "AC17F4207E045D532827013C122F6A6300E007E9"
 
-      expect(utorrent_stub).to receive(:get_torrent_job_properties).with(hash)
+      expect(adapter_mock).to receive(:get_torrent_job_properties).with(hash)
       app.get_torrent_job_properties(hash)
     end
-  end
 
-  context "#set_job_properties" do
-
-    it "sets job properties of a torrent" do
+    it "#set_job_properties" do
       props = {}
-      hash = "AC17F4207E045D532827013C122F6A6300E007E9"
-      props[hash] = {"seed_override" => 1, "seed_ratio" => 250}
+      props['hash1'] = {"seed_override" => 1, "seed_ratio" => 250}
 
-      expect(utorrent_stub).to receive(:set_job_properties).with(props)
+      expect(adapter_mock).to receive(:set_job_properties).with(props)
       app.set_job_properties(props)
     end
-  end
 
-  context "#torrents_removed?" do
-
-    it "determine if any torrents have been removed from the app" do
-      expect(utorrent_stub).to receive(:torrents_removed?)
+    it "#torrents_removed?" do
+      expect(adapter_mock).to receive(:torrents_removed?)
       app.torrents_removed?
     end
-  end
 
-  context "#removed_torrents" do
-
-    it "return list of removed torrents" do
-      expect(utorrent_stub).to receive(:removed_torrents)
+    it "#removed_torrents" do
+      expect(adapter_mock).to receive(:removed_torrents)
       app.removed_torrents
     end
-  end
 
-  context "#torrents" do
-
-    it "return list of cached torrents" do
-      expect(utorrent_stub).to receive(:torrents)
+    it "#torrents" do
+      expect(adapter_mock).to receive(:torrents)
       app.torrents
     end
-  end
 
-  context "#remove_torrent" do
-
-    it "remove a torrent" do
-      hash = "AC17F4207E045D532827013C122F6A6300E007E9"
-      expect(utorrent_stub).to receive(:remove_torrent).with(hash)
+    it "#remove_torrent" do
+      hash = "hash1"
+      expect(adapter_mock).to receive(:remove_torrent).with(hash)
       app.remove_torrent hash
     end
-  end
 
-  context "#get_torrent_seed_ratio" do
-
-    it "return a torrent's seed ratio" do
-      hash = "AC17F4207E045D532827013C122F6A6300E007E9"
-      expect(utorrent_stub).to receive(:get_torrent_job_properties).with(hash)
+    it "#get_torrent_seed_ratio" do
+      hash = "hash1"
+      expect(adapter_mock).to receive(:get_torrent_seed_ratio).with(hash, 0)
       app.get_torrent_seed_ratio hash, 0
     end
-  end
 
-  context "#apply_seed_limits" do
-
-    it "apply tracker filter seed limits to a collection of torrents" do
+    it "#apply_seed_limits" do
       torrents_to_limit = [
         { hash: 'hash1', name: 'torrent1' },
         { hash: 'hash2', name: 'torrent2' },
@@ -142,33 +119,22 @@ describe TorrentApp do
         { url: 'url2', limit: 41 },
       ]
 
-      expect(utorrent_stub).to receive(:get_torrent_job_properties).with('hash1')
-      expect(utorrent_stub).to receive(:get_torrent_job_properties).with('hash2')
-      expect(utorrent_stub).to receive(:get_torrent_job_properties).with('hash3')
+      expect(adapter_mock).to receive(:apply_seed_limits).with(torrents_to_limit, filters)
       app.apply_seed_limits torrents_to_limit, filters
     end
-  end
 
-  context "#settings" do
-
-    it "return torrent app settings" do
-      expect(utorrent_stub).to receive(:get_utorrent_settings)
+    it "#settings" do
+      expect(adapter_mock).to receive(:settings)
       app.settings
     end
-  end
 
-  context "#rssfilters" do
-
-    it "return rss filters from torrent app" do
-      expect(utorrent_stub).to receive(:rssfilters)
+    it "#rssfilters" do
+      expect(adapter_mock).to receive(:rssfilters)
       app.rssfilters
     end
-  end
 
-  context "#rssfeeds" do
-
-    it "return rss feeds from torrent app" do
-      expect(utorrent_stub).to receive(:rssfeeds)
+    it "#rssfeeds" do
+      expect(adapter_mock).to receive(:rssfeeds)
       app.rssfeeds
     end
   end
